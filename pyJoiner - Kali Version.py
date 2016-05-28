5d04a54697ffa355fbb2ac981651dab2f3926093
danielhnmoreno <danielhnmoreno@users.noreply.github.com>
2016-05-28 17:10:47 -0300

    Update pyJoiner - Kali Version.py

--- pyJoiner - Kali Version.py
+++ pyJoiner - Kali Version.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3.4
 #
-# pyJoiner - Kali Linux Version
-# Open Source Exe Joiner
+# pyJoiner - Open Source Exe Joiner (Kali Linux Version)
 # Coder: Daniel Henrique Negri Moreno (a.k.a W1ckerMan)
 #
-# pyJoiner is used for join files, similar with SFX (Self-extracting archive), but the major difference
-# is that pyJoiner silenty extracts joined files in %TEMP% directory and
-# execute them.
-# 
-# After process, /root/pyJoiner_output/py_file.exe will be generated. 
-# The output py_file.exe file must be execute under Windows platform.
-# 
+# pyJoiner is used for join files , like Self-extracting archive (SFX).
 #
+# The major difference resides in fact that pyJoiner extracts files
+# in %TEMP% Windows directory and executes them.
+#
+# After process, "/root/pyJoiner_output/py_file.exe" will be generated. 
+# The output "py_file.exe" file must be execute under Windows platform.
+# 
 # Usage: 
 # root@kali# python3.4 pyJoiner.py
 
